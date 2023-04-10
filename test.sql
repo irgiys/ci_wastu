8a3953ff0d2027b9598263cf1c5b9b70a6cf4836
irgiys <irgiyansy@gmail.com>
2023-04-10 21:41:25 +0700

    pertemuan 6

---
+++ test.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_mhs (nama, nim, alamat) VALUES  
+('Elfonda Mekel', 181351122, 'Jakarta'),
+('Akhdiyat Duta', 181352233, 'Yogyakarta'),
+('Kidnep', 181355533, 'Malang'),
+('Ari Lasso', 181357788, 'Surabaya'),
+('Rudi', 181359966, 'Bandung');
